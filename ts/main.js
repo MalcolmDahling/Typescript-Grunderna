fb6ef9c9da5bca0b77d65f30d30c728c0a693fde
MalcolmDahling <mac.dahling@gmail.com>
2022-01-18 11:43:57 +0100

    done

---
+++ ts/main.js
@@ -0,0 +1,18 @@
+window.onload = function () {
+    var firstName = 'Malcolm';
+    var age = 28;
+    console.log('Name: ' + firstName + ', Age: ' + age);
+    var numArr = [1, 2, 3, 4, 5];
+    for (var i = 0; i < numArr.length; i++) {
+        console.log(numArr[i]);
+        var p = document.createElement('p');
+        p.innerText = numArr[i].toString();
+        document.body.appendChild(p);
+    }
+    var bool = true;
+    console.log(bool);
+    var strArr = ['a', 'b', 'c'];
+    for (var i = 0; i < strArr.length; i++) {
+        console.log(strArr[i]);
+    }
+};
---
+++ ts/main.ts
@@ -0,0 +1,34 @@
+window.onload = function(){ //får error "cannot redeclare block-scoped variable" utan window.onload
+
+    let firstName:string = 'Malcolm';
+    let age:number = 28;
+    
+    console.log('Name: ' + firstName + ', Age: ' + age);
+    
+    
+    
+    
+    let numArr: number[] = [1, 2, 3, 4, 5];
+    
+    for(let i = 0; i < numArr.length; i++){
+        console.log(numArr[i]);
+    
+        let p = document.createElement('p');
+        p.innerText = numArr[i].toString();
+        document.body.appendChild(p);
+    }
+    
+    
+    
+    let bool:boolean = true;
+    console.log(bool);
+    
+    
+    let strArr:string[] = ['a', 'b', 'c'];
+    for (let i = 0; i < strArr.length; i++) {
+        console.log(strArr[i]);
+    }
+
+}
+
+
