c6a5a54d2e4035fc6c9edba4c1bdbd93fb589312
MalcolmDahling <mac.dahling@gmail.com>
2022-01-19 15:26:37 +0100

    Update main.js

--- ts/main.js
+++ ts/main.js
@@ -1,18 +1,17 @@
-window.onload = function () {
-    var firstName = 'Malcolm';
-    var age = 28;
-    console.log('Name: ' + firstName + ', Age: ' + age);
-    var numArr = [1, 2, 3, 4, 5];
-    for (var i = 0; i < numArr.length; i++) {
-        console.log(numArr[i]);
-        var p = document.createElement('p');
-        p.innerText = numArr[i].toString();
-        document.body.appendChild(p);
-    }
-    var bool = true;
-    console.log(bool);
-    var strArr = ['a', 'b', 'c'];
-    for (var i = 0; i < strArr.length; i++) {
-        console.log(strArr[i]);
-    }
-};
+//får error "cannot redeclare block-scoped variable" utan window.onload
+var firstName = 'Malcolm';
+var age = 28;
+console.log('Name: ' + firstName + ', Age: ' + age);
+var numArr = [1, 2, 3, 4, 5];
+for (var i = 0; i < numArr.length; i++) {
+    console.log(numArr[i]);
+    var p = document.createElement('p');
+    p.innerText = numArr[i].toString();
+    document.body.appendChild(p);
+}
+var bool = true;
+console.log(bool);
+var strArr = ['a', 'b', 'c'];
+for (var i = 0; i < strArr.length; i++) {
+    console.log(strArr[i]);
+}
